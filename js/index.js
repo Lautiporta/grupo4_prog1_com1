8627bebc7939dfd0804059482df431da5336c3f8
lautaropoggi <lpoggigamardo@udesa.edu.ar>
2024-11-22 10:52:39 -0300

    CSS(diseño y responsive)

--- js/index.js
+++ js/index.js
@@ -16,18 +16,15 @@ function obtenerRecetas() {
 
             for (let i = cantidadRecetas; i < cantidadRecetas + 10 && i < arrayDeRecetas.length; i++) {
                 allRecipesHTML += `<article class="productos">
-                                    <div class="detalles">
-                                        <img src="${arrayDeRecetas[i].image}" alt="${arrayDeRecetas[i].name}">
-                                        <p class="nombre">${arrayDeRecetas[i].name}</p>
-                                        <p>Dificultad: ${arrayDeRecetas[i].difficulty}</p>
-                                    </div>
-                                    <div class="productosLink">
-                                        <a class="detalles" href="receta.html?id=${arrayDeRecetas[i].id}">Detalle</a>
-                                    </div>
-                                  </article>`;
+                 <div class="detalles1"> 
+                 <img src="${arrayDeRecetas[i].image}" alt="${arrayDeRecetas[i].name}"> 
+                 <p class="nombre">${arrayDeRecetas[i].name}</p>
+                  <p>Dificultad: ${arrayDeRecetas[i].difficulty}</p> 
+                  </div> <div class="productosLink"> 
+                  <a class="detalles2" href="receta.html?id=${arrayDeRecetas[i].id}">Detalle</a> </div> </article>`;;
             }
 
-            
+             
             seccion.innerHTML += allRecipesHTML;
 
             
