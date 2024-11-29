7db3ab783046941089c8d7b4b3cb5a27fca409b9
lautaropoggi <lpoggigamardo@udesa.edu.ar>
2024-11-29 17:54:38 -0300

    cambio de estilo category y search-results, ultimo chequeo

--- js/index.js
+++ js/index.js
@@ -37,29 +37,3 @@ document.querySelector('.cargar-mas').addEventListener('click', function(e) {
 
 obtenerRecetas();
 
-
-
-let formulario = document.querySelector(".formBuscar");  
-let campoBusqueda = document.querySelector('#searchInput'); 
-let errorBusqueda = document.querySelector('.invalid-feedback-searchInput');  
-
-formulario.addEventListener('submit', function(event){
-    event.preventDefault();
-    let valid = true;
-    
-    if (campoBusqueda.value === "") {
-        errorBusqueda.innerText = 'Por favor complete el campo de búsqueda';
-        errorBusqueda.style.display = 'block';
-        valid = false;
-    }
-    
-    else if (campoBusqueda.value.length < 3) {
-        errorBusqueda.innerText = 'El término de búsqueda debe tener al menos 3 caracteres';
-        errorBusqueda.style.display = 'block';
-        valid = false;
-    } 
-
-    
-    if (valid) {
-        formulario.submit();
-}});
